4d089c62eb8057fa9b024205e43b37324e6c8716
yennylsamson <72492765+yennylsamson@users.noreply.github.com>
2021-11-07 19:32:24 +0800

    Fixed Subjects setters getters

--- backend/src/main/java/io/ims/backend/Models/Subjects.java
+++ backend/src/main/java/io/ims/backend/Models/Subjects.java
@@ -14,4 +14,53 @@ public class Subjects {
     public String units;
     public int lectureHours;
     public int labHours;
+
+    public int getSubjectID() {
+        return this.subjectID;
+    }
+
+    public void setSubjectID(int subjectID) {
+        this.subjectID = subjectID;
+    }
+
+    public String getSubjectName() {
+        return this.subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getSubjectCode() {
+        return this.subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public String getUnits() {
+        return this.units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    public int getLectureHours() {
+        return this.lectureHours;
+    }
+
+    public void setLectureHours(int lectureHours) {
+        this.lectureHours = lectureHours;
+    }
+
+    public int getLabHours() {
+        return this.labHours;
+    }
+
+    public void setLabHours(int labHours) {
+        this.labHours = labHours;
+    }
+
 }
