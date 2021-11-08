d95dc7f84a5fe47c8ea57dc9ef345d27c23c8a26
yennylsamson <72492765+yennylsamson@users.noreply.github.com>
2021-11-08 12:28:59 +0800

    Added User Details Controller

--- backend/src/main/java/io/ims/backend/Models/UserDetails.java
+++ backend/src/main/java/io/ims/backend/Models/UserDetails.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDetails extends User {
+public class UserDetails{
   @Id
   @GeneratedValue
   private int detailsID;
