b39ec74a113e87bea0eb73f4c1a37413eccd12c2
rphlmnbt <rphlmnbt@gmail.com>
2021-11-17 01:10:18 +0800

    Change repository method to allow more than one result to be obtained.

--- src/main/java/io/ims/backend/Repository/ActivityRepository.java
+++ src/main/java/io/ims/backend/Repository/ActivityRepository.java
@@ -6,14 +6,15 @@ import org.springframework.stereotype.Repository;
 
 import io.ims.backend.Models.Activity;
 
+import java.util.List;
 import java.util.Optional;
 
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long>{
     @Query("SELECT a FROM Activity a WHERE a.professor.userID = ?1")
-    Optional<Activity> findActivityByProfessorID(Long professorID);
+    List<Activity> findActivityByProfessorID(Long professorID);
 
     @Query("SELECT a FROM Activity a WHERE a.student.userID = ?1")
-    Optional<Activity> findActivityByStudentID(Long studentID);
+    List<Activity> findActivityByStudentID(Long studentID);
 }
