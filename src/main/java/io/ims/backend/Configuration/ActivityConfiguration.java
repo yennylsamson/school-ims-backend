0b317f49467926ffd842f46f94d93997064d6016
rphlmnbt <rphlmnbt@gmail.com>
2021-11-16 20:20:00 +0800

    Created a one-to-many relationship for students/prof to activities.

--- src/main/java/io/ims/backend/Configuration/ActivityConfiguration.java
+++ src/main/java/io/ims/backend/Configuration/ActivityConfiguration.java
@@ -1,33 +1,39 @@
 package io.ims.backend.Configuration;
 
 import io.ims.backend.Models.Activity;
+import io.ims.backend.Models.Professor;
+import io.ims.backend.Models.Student;
 import io.ims.backend.Repository.ActivityRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
-@Configuration
-public class ActivityConfiguration {
-    @Bean
-    CommandLineRunner activityLineRunner(ActivityRepository activityRepository){
-        return args -> {
-            Activity activity1 = new Activity(
-                    1L,
-                    "Activity 1",
-                    "Multiple Choice",
-                    24,
-                    30
-            );
-            Activity activity2 = new Activity(
-                    2L,
-                    "Activity 2",
-                    "Identification",
-                    28,
-                    30
-            );
-            activityRepository.saveAll(List.of(activity1,activity2));
-        };
-    }
-}
+//@Configuration
+//public class ActivityConfiguration {
+//    @Bean
+//    CommandLineRunner activityLineRunner(ActivityRepository activityRepository){
+//        return args -> {
+//            Activity activity1 = new Activity(
+//                    "Activity 1",
+//                    "Assignment",
+//                    24,
+//                    30,
+//                    ailger,
+//                    niyo
+//            );
+//            Activity activity2 = new Activity(
+//                    "Activity 2",
+//                    "Assignment",
+//                    28,
+//                    30,
+//                    ailger,
+//                    niyo
+//            );
+//            activityRepository.saveAll(List.of(activity1,activity2));
+//        };
+//    }
+//}
