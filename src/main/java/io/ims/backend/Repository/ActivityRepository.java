01de7ac40717ad9c370c9aa0c8b4832dfdc8718d
rphlmnbt <rphlmnbt@gmail.com>
2021-11-17 01:00:40 +0800

    Created Query for Finding Activity by Student/Professor ID

--- src/main/java/io/ims/backend/Repository/ActivityRepository.java
+++ src/main/java/io/ims/backend/Repository/ActivityRepository.java
@@ -1,12 +1,19 @@
 package io.ims.backend.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import io.ims.backend.Models.Activity;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long>{
-    
+    @Query("SELECT a FROM Activity a WHERE a.professor.userID = ?1")
+    Optional<Activity> findActivityByProfessorID(Long professorID);
+
+    @Query("SELECT a FROM Activity a WHERE a.student.userID = ?1")
+    Optional<Activity> findActivityByStudentID(Long studentID);
 }
