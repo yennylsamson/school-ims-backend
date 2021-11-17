01bc552c49360c9d147ebcb9530ad158b4c855e9
rphlmnbt <rphlmnbt@gmail.com>
2021-11-18 00:00:17 +0800

    Created method for updating user and user details.

--- src/main/java/io/ims/backend/Services/UserService.java
+++ src/main/java/io/ims/backend/Services/UserService.java
@@ -1,21 +1,26 @@
 package io.ims.backend.Services;
 
-import io.ims.backend.Models.Professor;
+import io.ims.backend.Models.Course;
 import io.ims.backend.Models.User;
-import io.ims.backend.Repository.ProfessorRepository;
+import io.ims.backend.Models.UserDetails;
+import io.ims.backend.Repository.UserDetailsRepository;
 import io.ims.backend.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserDetailsRepository userDetailsRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserDetailsRepository userDetailsRepository) {
         this.userRepository = userRepository;
+        this.userDetailsRepository = userDetailsRepository;
     }
 
     public User getUserByEmail(String email) {
@@ -34,6 +39,84 @@ public class UserService {
         }
 
         return loggedIn;
+    }
+
+    @Transactional
+    public void updateUser(Long userID,
+                              String email,
+                              String password,
+                              String userRole,
+                              String firstName,
+                              String lastName,
+                              String gender,
+                              LocalDate birthDate,
+                              String homeAddress,
+                              String contactNumber,
+                              String civilStatus) {
+
+        UserDetails userDetails = userDetailsRepository.findById(userID)
+                .orElseThrow(() -> new IllegalStateException(
+                        "user with id " + userID + " does not exists"
+                ));
+
+        if (email != null &&
+                email.length() > 0 &&
+                !Objects.equals(userDetails.getEmail(), userDetails)) {
+            userDetails.setEmail(email);
+        }
+
+        if (password != null &&
+                password.length() > 0 &&
+                !Objects.equals(userDetails.getPassword(), userDetails)) {
+            userDetails.setPassword(password);
+        }
+
+        if (userRole != null &&
+                userRole.length() > 0 &&
+                !Objects.equals(userDetails.getUserRole(), userDetails)) {
+            userDetails.setUserRole(userRole);
+        }
+
+        if (firstName != null &&
+                firstName.length() > 0 &&
+                !Objects.equals(userDetails.getFirstName(), userDetails)) {
+            userDetails.setFirstName(firstName);
+        }
+
+        if (lastName != null &&
+                lastName.length() > 0 &&
+                !Objects.equals(userDetails.getLastName(), userDetails)) {
+            userDetails.setLastName(lastName);
+        }
+
+        if (gender != null &&
+                gender.length() > 0 &&
+                !Objects.equals(userDetails.getGender(), userDetails)) {
+            userDetails.setGender(gender);
+        }
+
+        if (birthDate != null &&
+                !Objects.equals(userDetails.getBirthDate(), userDetails)) {
+            userDetails.setBirthDate(birthDate);
+        }
+
+        if (homeAddress != null &&
+                homeAddress.length() > 0 &&
+                !Objects.equals(userDetails.getHomeAddress(), userDetails)) {
+            userDetails.setHomeAddress(homeAddress);
+        }
+
+        if (contactNumber != null &&
+                !Objects.equals(userDetails.getContactNumber(), userDetails)) {
+            userDetails.setContactNumber(contactNumber);
+        }
+
+        if (civilStatus != null &&
+                civilStatus.length() > 0 &&
+                !Objects.equals(userDetails.getCivilStatus(), userDetails)) {
+            userDetails.setCivilStatus(civilStatus);
+        }
+
     }
 
 }
